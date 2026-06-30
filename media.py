5f4e5f43125a2e6590e78c3568ff0ac947cfd6e7
Thales Henrique <thaleshenriquevieiragomes146@gmail.com>
2026-06-30 16:52:33 -0300

    Apagando Funcao desnecessaria

--- media.py
+++ media.py
@@ -5,14 +5,3 @@ def converter_notas(nota: str):
         return None
     
     return float(nota.replace(",","."))
-
-    
-def calcular_media(materia: str, notas: dict):
-    notas_validas = [n for n in notas[materia] if n is not None]
-
-    if not notas_validas:
-        return f"{materia}: Sem notas"
-    
-    media = round(sum(notas_validas) / len(notas_validas), 2)
-
-    return f"Materia: {materia} \n Bimestres concluidos: {len(notas_validas)} \n  Media: {media}"
